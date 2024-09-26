bfd81c6ea939243826fbc4ff81f6d7b76bf3572a
avgnpc <5025221059@student.its.ac.id>
2024-09-26 13:46:27 -0700

    feat: view data

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,54 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Surya Alamsyah',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.'
+        ],
+        [
+            'id' => '2',
+            'slug' => 'Judul Artikel 2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Surya Alamsyah',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Surya Alamsyah',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.'
+        ],
+        [
+            'id' => '2',
+            'slug' => 'Judul Artikel 2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Surya Alamsyah',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/about', function () {
